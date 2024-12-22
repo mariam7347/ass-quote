d89a70decc232e9020f85b1a1aa244c4c6a8c19f
mariam <mariamayman203@gmail.com>
2024-12-22 18:02:17 +0200

    first one

---
+++ qoute.js
@@ -0,0 +1,6 @@
+function getQuote(){
+    document.getElementById('paraQuote').innerHTML='<p id="paraQuote" class="m-5 d-block">“Be yourself; everyone else is already taken.”</p>'
+    document.getElementById('capQuote').innerHTML='<p id="capQuote" class="m-5 fs-3">― Oscar Wilde</p>'
+
+}
+
